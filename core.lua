54a90bc37ee9e9c8847ec9df4ae23189f996aa4e
Brandon Jernigan <brandon@whatisboom.com>
2024-07-09 21:27:05 -0500

    fix: moving roster frame creation earlier in the process

--- core.lua
+++ core.lua
@@ -19,21 +19,22 @@ function MythicMonday:Init()
   SLASH_MythicMonday1 = "/mm"
   SlashCmdList[addonName] = SlashCommand
   MythicMonday:CreateMainFrame()
+  MythicMonday:CreateRosterContainer()
 end
 
 function MythicMonday:Render()
   MythicMonday:CreateGroupsContainer()
-  -- local guildIndex = 1
-  -- for i=1,4 do
-  --   local groupFrame = MythicMonday:GetGroupFrame()
-  --   for j=1,5 do
-  --     local name, _, _, _, class = GetGuildRosterInfo(guildIndex)
-  --     if not name then return end
-  --     local player = MythicMonday:GetGroupPlayerFrame(groupFrame, name, class, guildIndex * 150) -- math.random(2000,3000))
-  --     guildIndex = guildIndex + 1
-  --   end
-  -- end
-  local roster = MythicMonday:CreateRosterContainer()
+  local guildIndex = 1
+  for i=1,4 do
+    local groupFrame = MythicMonday:GetGroupFrame()
+    for j=1,5 do
+      local name, _, _, _, class = GetGuildRosterInfo(guildIndex)
+      if not name then return end
+      local player = MythicMonday:GetGroupPlayerFrame(groupFrame, name, class, guildIndex * 150) -- math.random(2000,3000))
+      guildIndex = guildIndex + 1
+    end
+  end
+  -- local roster = MythicMonday:CreateRosterContainer()
   -- for i=41,51 or GetNumGuildMembers() do
   --   local name, _, _, _, class = GetGuildRosterInfo(i)
   --   local frame = MythicMonday:GetRosterPlayerFrame(roster, name, class, math.random(2000,3000))
